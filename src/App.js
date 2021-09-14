c9339755b1c7c1c21b5f5dc72573c3b2584a4080
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-13 21:18:18 -0300

    criando Routes para navegação simple page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path="/" component={ Home } />
+        <Route path="/About" component={ About } />
+        <Route path="/Users" component={ Users } />
+
+      </BrowserRouter>
     );
   }
 }
