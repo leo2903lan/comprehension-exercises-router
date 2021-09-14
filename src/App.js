450f8016ebf06ca884eacf87e27ff0114af2e131
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-13 21:24:53 -0300

    criando links de navegação entre as rotas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to="/" >Home</Link>
+        <Link to="/About" >About</Link>
+        <Link to="/Users" >User</Link>
         <Route exact path="/" component={ Home } />
         <Route path="/About" component={ About } />
         <Route path="/Users" component={ Users } />
